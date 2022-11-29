afe7e303c758dabf945b096a3e26be6c92675fdb
Jonathan Zehner <jonathan.zehner@ufl.edu>
2022-11-29 14:10:26 -0500

    Style footer at mobile width

--- footer.php
+++ footer.php
@@ -2,11 +2,34 @@
             <footer class="footer">
                 <div class="container">
                     <div class="row">
-                    <!-- Left Footer Menu Widget -->
-                        <div class="col-md-3 footer-widget-container">
-                            <?php
-                                dynamic_sidebar('left-footer-widget');
-                            ?>                        
+
+                    <!-- Left Footer Widget and Menu -->
+                        <div class="col-md-3 left-footer-container">
+
+                        <!-- Left Footer Widget -->
+                            <div class="left-footer-widget">
+                                <?php
+                                    dynamic_sidebar('left-footer-widget');
+                                ?>                        
+                            </div>
+
+                        <!-- Left Footer Menu -->
+                            <div class="left-footer-menu">
+                                <?php // Create conditional loop for footer menu
+                                    if(has_nav_menu('footer-left')){ ?>
+                                        <nav class="footer-menu">
+                                            <?php // Create Navigation Menu
+                                                wp_nav_menu(array(
+                                                    // Key                      Value
+                                                    'theme_location'   => 'footer-left',
+                                                ));
+                                            ?>
+                                        </nav><?php
+                                    } else {
+                                        echo "<p>Please select a menu through the dashboard</p>";
+                                    }
+                                ?>
+                            </div>
                         </div>
 
                     <!-- Middle Footer Widget and Menu -->
@@ -37,12 +60,39 @@
                             </div>
                         </div>
 
-                    <!-- Right Footer Menu Widget -->
-                        <div class="col-md-3 footer-widget-container">
-                            <?php
-                                dynamic_sidebar('right-footer-widget');
-                            ?>                        
+                    <!-- Right Footer Widget and Menu -->
+                        <div class="col-md-3 right-footer-container">
+
+                        <!-- Right Footer Menu -->
+                            <div class="right-footer-menu">
+                                <?php // Create conditional loop for footer menu
+                                    if(has_nav_menu('footer-right')){ ?>
+                                        <nav class="footer-menu">
+                                            <?php // Create Navigation Menu
+                                                wp_nav_menu(array(
+                                                    // Key                      Value
+                                                    'theme_location'   => 'footer-right',
+                                                ));
+                                            ?>
+                                        </nav><?php
+                                    } else {
+                                        echo "<p>Please select a menu through the dashboard</p>";
+                                    }
+                                ?>
+                            </div>
+                        <!-- Right Footer Widget -->
+                            <div class="right-footer-widget">
+                                <?php
+                                    dynamic_sidebar('right-footer-widget');
+                                ?>                        
+                            </div>
                         </div>
+                        
+                        <div class="footer-meta">
+                            <h2>Website Created by Nomad Web Designs</h2>
+                            <p>Copyright &copy; 2022, all rights reserved.</p>
+                        </div>
+
                     </div>
                 </div>
             </footer>
